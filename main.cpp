7a76a463ea575e2611c8399d9a29304fb6d1b2f0
cbgaors120 <bgaor@students.avc.edu>
2026-03-27 15:03:55 -0700

    A52 Assignment. Max value finder.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     // TODO
+    int value, maxVal;
 
+    for (int i=0;i<10;i++){  // initialize, condition (while this .. keep going/boundary), update 
+        cin >> value;
+
+        if (value > maxVal || i == 0)
+            maxVal = value;
+
+            
+    }    
+    
+        cout << maxVal  << " is the max value number." << endl;
+    
+        
     return 0;
 }
